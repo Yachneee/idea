a207f99dc2b4352d7cd88f830729500f17673652
Yachne <3291301890@qq.com>
2023-08-01 21:50:04 +0800

    2023.8.1

---
+++ day01/Demo1.java
@@ -0,0 +1,21 @@
+public class Demo{
+public static void main(String[] args){
+//1.整数
+System.out.println(666);
+//2.小数
+System.out.println(3.66);
+//3.字符: 字符必须用单引号引起来
+System.out.println('a');
+System.out.println('0');
+System.out.println('中');
+System.out.println(' '); //空格也算字符
+// 特殊字符：\t表示制表符 \n表示换行
+System.out.println('\t'); //这相当于一个tab键，专业叫做制表符
+System.out.println('\n'); //这是换行的意思
+//4.字符串：字符串是双引号引起来的
+System.out.println("我爱你中国abc00.@#$%");
+//5.布尔值：只有两个值true和false
+System.out.println(true);
+System.out.println(false);
+}
+}
---
+++ day01/calculate.java
@@ -0,0 +1,27 @@
+public class calculate{
+public static void main(String[] args){
+System.out.println("ÄãºÃ");
+
+//再收20元
+money = money + 20;
+//再打印money记录的值是多少
+System.out.println(money); // 108.8
+// 再比如：公交车大人2元，小孩一元，公交车经过2站
+// 第一站：3个大人1个小孩上车 3 * 2 + 1
+// 第二站：1个大人1个小孩上车 1 * 2 + 1
+// 请问一共上了多少人，一共收入多少钱？
+// 刚开始公交车箱子里没有钱，用money2表示箱子里的钱
+int money2 = 0;
+//刚开始公交车上也没有人，用count变量表示上车的人数
+int count = 0;
+//经过两站的人数：第一站3人，第二站1人，总的人数是3+1
+count = 3 + 1;
+count = count + 1 + 1;
+//经过两站的钱数：
+money2 = money2+ 3*2+1; // 经过第一站后
+money2 = money2+2+1; // 经过第二站后
+// 打印人数和钱数
+System.out.println(count); // 6
+System.out.println(money); // 10
+}
+}
