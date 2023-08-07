9d52fba3f4520d35cff627f36b3cd1af654cdc5a
Yachne <3291301890@qq.com>
2023-08-07 14:06:31 +0800

    2023.8.7  整理文件夹

--- day01/IsocelesTriangle.java
+++
@@ -1,9 +0,0 @@
-public class IsocelesTriangle{
-	public static void main(String[] args){
-	System.out.println("    *");
-	System.out.println("   ***");
-	System.out.println("  *****");
-	System.out.println(" *******");
-	System.out.println("*********");
-	}
-}
