e73755e769682dad717d26e1d87e27489922a6ed
Yachne <3291301890@qq.com>
2023-08-10 14:10:57 +0800

    8.8 增强for语句

--- week4/src/day2_808/exercise/Exercise7.java
+++ week4/src/day2_808/exercise/Exercise7.java
@@ -1,9 +1,9 @@
 package day2_808.exercise;
 
 import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
-
+/**
+ * @author Administrator
+ */
 public class Exercise7 {
     public static void main(String[] args) {
         //给定两个各不包含重复元素的数组 arr 和 arr1 ，求 arr 和 arr1 的交集(在 arr 中有，arr1 中也包含的元素集合)，
@@ -11,31 +11,30 @@ public class Exercise7 {
         int[] arr= {1, 2, 3};
         int[] arr1= {2, 3};
         int len=0;
-        for (int i = 0; i < arr.length; i++) {
-            int count=0;
-            for (int j = 0; j < arr1.length; j++) {
-                if(arr[i]==arr1[j]){
+        for (int k : arr) {
+            int count = 0;
+            for (int i : arr1) {
+                if (k == i) {
                     count++;
                 }
             }
-            if(count>0){
+            if (count > 0) {
                 len++;
             }
         }
         System.out.println("arr与arr1相同的元素个数："+len);
         int[] same=new int[len];
         int sIndex=-1;
-        for (int i = 0; i < arr.length; i++) {
-            int count=0;
-            int current=arr[i];
-            for (int j = 0; j < arr1.length; j++) {
-                if(current==arr1[j]){
+        for (int k : arr) {
+            int count = 0;
+            for (int i : arr1) {
+                if (k == i) {
                     count++;
                 }
             }
-            if(count>0){
+            if (count > 0) {
                 sIndex++;
-                same[sIndex]=arr[i];
+                same[sIndex] = k;
             }
         }
         System.out.println("数组arr与数组arr1的交集："+Arrays.toString(same));
