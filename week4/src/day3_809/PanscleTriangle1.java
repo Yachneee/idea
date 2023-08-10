699c7a7b07d88db73dc3b533d41808b9bcddae3b
Yachne <3291301890@qq.com>
2023-08-10 14:33:35 +0800

    杨辉三角

--- week4/src/day3_809/PanscleTriangle1.java
+++ week4/src/day3_809/PanscleTriangle1.java
@@ -2,34 +2,25 @@ package day3_809;
 
 import java.util.Scanner;
 
-public class PanscleTriangle {
+public class PanscleTriangle1 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("请输入行数：");
         int n=sc.nextInt();
-        int[][] tr=new int[n][n];
+        int[][] tr=new int[n][];
         for (int i = 0; i < tr.length; i++) {
-//            for (int j = 0; j < tr[i].length; j++) {
-//                if(j==0||j==i){
-//                    tr[i][j]=1;
-//                }else if(i>0){
-//                    tr[i][j]=tr[i-1][j]+tr[i-1][j-1];
-//                }
-//            }
-            for (int j = 0; j <i+1; j++) {
-                if(j==0||j==i){
+            tr[i]=new int[i+1];
+            for (int j = 0; j < tr[i].length; j++) {
+                if(j==0||j==tr[i].length-1){
                     tr[i][j]=1;
                 }else{
                     tr[i][j]=tr[i-1][j]+tr[i-1][j-1];
                 }
             }
-
         }
         for (int[] r : tr) {
             for (int c : r) {
-                if(c!=0){
-                    System.out.print(c+"\t");
-                }
+                System.out.print(c+"\t");
             }
             System.out.println();
         }
