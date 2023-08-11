09d0aa56cdcfc13af65df808448d028e04d3ca4b
Yachne <3291301890@qq.com>
2023-08-11 11:12:37 +0800

    8.11 am 修改一下登陆验证方法

--- week4/src/day4_810/exercise/Exercise3Log.java
+++ week4/src/day4_810/exercise/Exercise3Log.java
@@ -1,27 +1,36 @@
 package day4_810.exercise;
-
-import java.util.Objects;
 import java.util.Scanner;
 
+/**
+ * @author Administrator
+ */
 public class Exercise3Log {
     public static void main(String[] args) {
-        logConfirm();
-    }
-
-    public static void logConfirm(){
-        Scanner sc=new Scanner(System.in);
-        int n=3;
-        for(int i=0;i<n;i++){
+        Scanner sc = new Scanner(System.in);
+        /* 定义允许的输入次数 */
+        int n = 3;
+        /* 循环，调用验证方法 */
+        while(true){
             System.out.print("请输入用户名：");
-            String name=sc.next();
+            String name = sc.next();
             System.out.print("请输入密码：");
-            String password=sc.next();
-            if(Objects.equals(name, "开发喵") && Objects.equals(password, "kaifamiao")){
-                System.out.println("登陆成功");
+            String password = sc.next();
+            if (logConfirm(name, password)) {
+                System.out.println("登陆成功！");
                 break;
-            }else{
-                System.out.println("你还有 "+(n-1-i)+" 次机会");
+            }else {
+                n--;
+                if(n==0){
+                    System.out.println("账号已被锁定！");
+                    break;
+                }else{
+                System.out.println("你还有 " + n + " 次机会！");
+                }
             }
         }
+    }
+
+    public static boolean logConfirm(String name,String password){
+        return "开发喵".equals(name) && "kaifamiao".equals(password);
     }
 }
