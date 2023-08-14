42037004a8336617bc1dd2976c0cc5866bbbf84c
Yachne <3291301890@qq.com>
2023-08-14 17:02:39 +0800

    8.14 更新一下素因子

--- week5/src/day1_814/PrimeDivisor.java
+++ week5/src/day1_814/PrimeDivisor.java
@@ -8,21 +8,29 @@ public class PrimeDivisor {
         System.out.print("请输入一个数字：");
         int num=sc.nextInt();
         System.out.print(num+"的素因子有：");
-        for (int i = 2; i <= num; i++) {
-            if(divisor(num,i)&&prime(i)){
-                System.out.print(i+"\t");
-            }
-        }
+//        for (int i = 2; i <= num; i++) {
+//            if(divisor(num,i)&&prime(i)){
+//                System.out.print(i+"\t");
+//            }
+//        }
+        primeDivisor(num);
     }
     public static boolean divisor(int num,int d){
         return num%d==0;
     }
     public static boolean prime(int d){
-        for (int i = 2; i <= d/2; i++) {
+        for (int i = 2; i < d; i++) {
             if(d%i==0){
                 return false;
             }
         }
         return true;
+    }
+    public static void primeDivisor(int num){
+        for(int i = 2; i <= num; i++){
+            if(num%i==0 && prime(i)){
+                System.out.print(i+"\t");
+            }
+        }
     }
 }
