2389c50f9522eb75572a639453e40614d6c76e6b
Yachne <3291301890@qq.com>
2023-08-25 12:39:11 +0800

    8.24 修改计算器bug

--- week6/src/day4_824/Calculator.java
+++ week6/src/day4_824/Calculator.java
@@ -10,68 +10,91 @@ public class Calculator {
         Scanner sc=new Scanner(System.in);
         System.out.print("请输入一个计算公式：");
         String str=sc.nextLine();
-        cal(str);
+        System.out.printf("%s=%s",str,cal(str));
         //1+2*3-4/2
         // =5
     }
-    public static void cal(String str){
-        String str1=mul(str);
-        String str2=dvi(str1);
-        String str3=add(str2);
-        String str4=reduce(str3);
-        System.out.printf("%s = %s = %s = %s = %s \n",str,str1,str2,str3,str4);
-        System.out.printf("%s = %s",str,str4);
+    public static String cal(String str){
+        while (str.contains("*")||str.contains("/")){
+            while(str.indexOf("/")<str.indexOf("*")|| !str.contains("*")){
+                str=dvi(str);
+                str=mul(str);
+            }
+            str=mul(str);
+            str=dvi(str);
+        }
+        while(str.contains("+")||str.contains("-")){
+            str=reduce(str);
+            str=add(str);
+        }
+        return str;
     }
     public static String mul(String str){
-        int i;
-        while(str.contains("*")){
-            i=str.indexOf("*");
-            char c1=str.charAt(i-1);
-            char c2=str.charAt(i+1);
-            int i1=Integer.parseInt(String.valueOf(c1));
-            int i2=Integer.parseInt(String.valueOf(c2));
-            String s=String.valueOf(i1*i2);
-            str=str.replace(c1+"*"+c2,s);
+        int index=str.indexOf("*");
+        if(index!=-1){
+            String str1=sLeft(str,index);
+            String str2=sRight(str,index);
+            String s=String.valueOf(Integer.parseInt(str1)*Integer.parseInt(str2));
+            str=str.replace(str1+"*"+str2,s);
         }
         return str;
     }
     public static String dvi(String str){
-        int i;
-        while(str.contains("/")){
-            i=str.indexOf("/");
-            char c1=str.charAt(i-1);
-            char c2=str.charAt(i+1);
-            int i1=Integer.parseInt(String.valueOf(c1));
-            int i2=Integer.parseInt(String.valueOf(c2));
-            String s=String.valueOf(i1/i2);
-            str=str.replace(c1+"/"+c2,s);
+        int index=str.indexOf("/");
+        if(index!=-1){
+            String str1=sLeft(str,index);
+            String str2=sRight(str,index);
+            String s=String.valueOf(Integer.parseInt(str1)/Integer.parseInt(str2));
+            str=str.replace(str1+"/"+str2,s);
         }
         return str;
     }
     public static String add(String str){
-        int i;
-        while(str.contains("+")){
-            i=str.indexOf("+");
-            char c1=str.charAt(i-1);
-            char c2=str.charAt(i+1);
-            int i1=Integer.parseInt(String.valueOf(c1));
-            int i2=Integer.parseInt(String.valueOf(c2));
-            String s=String.valueOf(i1+i2);
-            str=str.replace(c1 +"+"+c2,s);
+        int index=str.indexOf("+");
+        if(index!=-1){
+            String str1=sLeft(str,index);
+            String str2=sRight(str,index);
+            String s=String.valueOf(Integer.parseInt(str1)+Integer.parseInt(str2));
+            str=str.replace(str1+"+"+str2,s);
         }
         return str;
     }
     public static String reduce(String str){
-        int i;
-        while(str.contains("-")){
-            i=str.indexOf("-");
-            char c1=str.charAt(i-1);
-            char c2=str.charAt(i+1);
-            int i1=Integer.parseInt(String.valueOf(c1));
-            int i2=Integer.parseInt(String.valueOf(c2));
-            String s=String.valueOf(i1-i2);
-            str=str.replace(c1+"-"+c2,s);
+        int index=str.indexOf("-");
+        if(index!=-1){
+            String str1=sLeft(str,index);
+            String str2=sRight(str,index);
+            String s=String.valueOf(Integer.parseInt(str1)-Integer.parseInt(str2));
+            str=str.replace(str1+"-"+str2,s);
         }
         return str;
+    }
+    public static String sLeft(String str,int index){
+        int d1LeftIndex=-1;
+        for (int i = index-1; i >= 0; i--) {
+            if(i==0){
+                d1LeftIndex=0;
+                break;
+            }
+            if(!(Character.isDigit(str.charAt(i)))){
+                d1LeftIndex=i+1;
+                break;
+            }
+        }
+        return str.substring(d1LeftIndex,index);
+    }
+    public static String sRight(String str,int index){
+        int d2RightIndex=-1;
+        for (int i=index+1; i<str.length(); i++) {
+            if(i==str.length()-1){
+                d2RightIndex=str.length();
+                break;
+            }
+            if(!(Character.isDigit(str.charAt(i)))){
+                d2RightIndex=i;
+                break;
+            }
+        }
+        return str.substring(index+1,d2RightIndex);
     }
 }
