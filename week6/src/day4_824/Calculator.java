5b0ebed80e277516a77f6901848f5fe44f8d9ac6
Yachne <3291301890@qq.com>
2023-08-25 15:16:23 +0800

    8.25 再次修改计算器bug

--- week6/src/day4_824/Calculator.java
+++ week6/src/day4_824/Calculator.java
@@ -14,9 +14,16 @@ public class Calculator {
         //1+2*3-4/2
         // =5
     }
+
+    /**
+     * 四则运算（做一些运算顺序的判断，有待改善，修改bug）
+     * @param str 要计算的公式
+     * @return 计算结果
+     */
     public static String cal(String str){
         while (str.contains("*")||str.contains("/")){
-            while(str.indexOf("/")<str.indexOf("*")|| !str.contains("*")){
+            while((str.contains("*") && str.contains("/") && str.indexOf("/")<str.indexOf("*"))
+                    || (!str.contains("*")&&str.contains("/"))){
                 str=dvi(str);
                 str=mul(str);
             }
@@ -29,6 +36,12 @@ public class Calculator {
         }
         return str;
     }
+
+    /**
+     * 计算乘法
+     * @param str 要计算的公式
+     * @return 将（数*数）替换为运算结果的字符串
+     */
     public static String mul(String str){
         int index=str.indexOf("*");
         if(index!=-1){
@@ -39,6 +52,12 @@ public class Calculator {
         }
         return str;
     }
+
+    /**
+     * 计算除法
+     * @param str 要计算的公式
+     * @return 将（数/数）替换为运算结果的字符串
+     */
     public static String dvi(String str){
         int index=str.indexOf("/");
         if(index!=-1){
@@ -49,6 +68,12 @@ public class Calculator {
         }
         return str;
     }
+
+    /**
+     * 计算加法
+     * @param str 要计算的公式
+     * @return 将（数+数）替换为运算结果的字符串
+     */
     public static String add(String str){
         int index=str.indexOf("+");
         if(index!=-1){
@@ -59,6 +84,12 @@ public class Calculator {
         }
         return str;
     }
+
+    /**
+     * 计算减法
+     * @param str 要计算的公式
+     * @return 将（数-数）替换为运算结果的字符串
+     */
     public static String reduce(String str){
         int index=str.indexOf("-");
         if(index!=-1){
@@ -69,6 +100,13 @@ public class Calculator {
         }
         return str;
     }
+
+    /**
+     * 运算符左边的的数字（传入的下标向左找第一个运算符，没有运算符就截取0到下标）
+     * @param str 查找的字符串
+     * @param index 运算符下标
+     * @return 左边数（字符串）
+     */
     public static String sLeft(String str,int index){
         int d1LeftIndex=-1;
         for (int i = index-1; i >= 0; i--) {
@@ -83,6 +121,13 @@ public class Calculator {
         }
         return str.substring(d1LeftIndex,index);
     }
+
+    /**
+     * 运算符右边的数字（传入的下标向右找第一个运算符，没有运算符就截取index+1到最后一个）
+     * @param str 查找的字符串
+     * @param index 运算符下标
+     * @return 右边的数（字符串）
+     */
     public static String sRight(String str,int index){
         int d2RightIndex=-1;
         for (int i=index+1; i<str.length(); i++) {
