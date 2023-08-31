830a49ce3a6794461bb9c3527863858bb658caa1
Yachne <3291301890@qq.com>
2023-08-31 17:17:46 +0800

    按字典分割字符串

--- week7/src/day3_830/pract/WordBreak.java
+++ week7/src/day3_830/pract/WordBreak.java
@@ -1,5 +1,8 @@
 package day3_830.pract;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * @author Administrator
  */
@@ -13,13 +16,46 @@ public class WordBreak {
                 s = s.substring(0, index1) + s.substring(index2);
             }
         }
-        return s.isEmpty()||s.isBlank();
+        StringBuilder str= new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if(Character.isLetter(s.charAt(i))){
+                str.append(s.charAt(i));
+            }
+        }
+        return str.length() == 0;
+    }
+    public static boolean wordBre(String s,String[] wordDict){
+        boolean[] booleans=new boolean[s.length()];
+        int index=0;
+        for (int i = 0; i < s.length(); i++) {
+            String str=s.substring(index,i+1);
+            for (String value : wordDict) {
+                if (str.equals(value)) {
+                    booleans[i] = true;
+                    index = i + 1;
+                    break;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(booleans));
+        return booleans[s.length()-1];
     }
     public static void main(String[] args) {
-        String string="hellocodecodenotsleep";
-        String[] wordDict= {"hello","sleep","code","hi","not"};
-        System.out.println(wordBreak(string, wordDict));
-        // 输出: true
+        String string="cars";
+        String[] wordDict= {"car","ca","rs"};
+        System.out.printf("%s %s 被拆分为字典中的单词\n",string,(wordBreak(string, wordDict)?"能":"不能"));
+        //不能
+        //TODO 有bug
+        System.out.printf("%s %s 被拆分为字典中的单词\n",string,(wordBre(string, wordDict)?"能":"不能"));
+        //能
+        String string1="hellocode";
+        String[] wordDict1= {"hello","code","he"};
+        System.out.printf("%s %s 被拆分为字典中的单词\n",string1,(wordBreak(string1, wordDict1)?"能":"不能"));
+        //能
+        System.out.printf("%s %s 被拆分为字典中的单词\n",string1,(wordBre(string1, wordDict1)?"能":"不能"));
+        //不能
+        //TODO 有bug
+
     }
 }
 
