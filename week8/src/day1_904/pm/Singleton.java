b942b2850e8097f580f648272bf2ae0be90ad73c
Yachne <3291301890@qq.com>
2023-09-04 15:56:49 +0800

    单例模式——枚举

---
+++ week8/src/day1_904/pm/Singleton.java
@@ -0,0 +1,18 @@
+package day1_904.pm;
+
+/**
+ * @author Administrator
+ */
+
+public enum Singleton {
+    /**
+     * 枚举实现单例模式，一个实例
+     */
+    instance;
+}
+class Test{
+    public static void main(String[] args) {
+        Singleton singleton=Singleton.instance;
+        System.out.println(singleton);
+    }
+}
